23d5eb075808cb360ccb8915b87e161a0e5da839
Ray Kim <msca8h@naver.com>
2018-12-28 22:04:27 +0900

    test script

--- LPBO/test2.jl
+++ LPBO/test2.jl
@@ -68,6 +68,46 @@ for i = 1:30
             " x: ", x_selected, " y: ", y_selected)
 end
 
+str = Vector{UInt8}(1024 * 1024);
+size = Ref{Int64}(0);
+println("serializing...")
+ccall((:test_serialize, "./libLPBO.so"), Void, (Ptr{Void}, Ref{UInt8}, Ref{Int64}), model, str, size)
+println("deserializing...")
+ccall((:test_deserialize, "./libLPBO.so"), Void, (Ptr{Void}, Ptr{UInt8}, Int64), model, str, size[])
+println("...done!")
+
+for i = 31:100
+    mean  = Ref{Float64}(0.0);
+    merit = Ref{Float64}(0.0);
+    var   = Ref{Float64}(0.0);
+
+    x_selected = ccall((:test_next_point, "./libLPBO.so"), Float64,
+                       (Ptr{Void}, Float64, Float64, Int64, Int64,
+                        Ref{Float64}, Ref{Float64}, Ref{Float64}),
+                       model, x_selected, y_selected, i, 200, merit, mean, var)
+    y_selected = objective(x_selected)
+
+    ccall((:test_render_acquisition, "./libLPBO.so"), Float64,
+          (Ptr{Void}, Ptr{Float64}, Ptr{Float64},
+           Int64, Int64, Ptr{Float64}, Ptr{Float64}, Int64),
+          model, x, y, samples, i, x_tilda, y_tilda, resolution)
+
+    plt.plot(x_tilda, y_tilda)
+    plt.plot(0.0:0.001:1.0, objective_noiseless(0.0:0.001:1.0))
+    plt.scatter(x, y, color="blue")
+    plt.scatter(x_selected, y_selected, color="red")
+    plt.show()
+
+    push!(x, x_selected)
+    push!(y, y_selected)
+    push!(mean_history, mean[])
+    push!(merit_history, merit[])
+    push!(var_history, var[])
+    samples += 1
+    println("iteration: ", i, " mean: ", mean[], " var: ", var[],
+            " x: ", x_selected, " y: ", y_selected)
+end
+
 plt.plot(1:30, merit_history)
 #plt.plot(1:30, var_history)
 plt.show()
