c6fc3c814fba9429a88c57196adb2bafcd17d477
Ray Kim <msca8h@naver.com>
2018-12-31 02:37:34 +0900

    updated test script to Julia 1.0 syntax

--- LPBO/test2.jl
+++ LPBO/test2.jl
@@ -2,112 +2,116 @@
 using PyCall
 @pyimport matplotlib.pyplot as plt
 
-eps = 2;
-function objective(x)
-    return 10 * sin(4 * π * x) +  eps * randn();
-end
+function main()
+    eps = 2;
+    function objective(x)
+        return 10 * sin(4.0 * π * x) +  eps * randn();
+    end
 
-function objective_noiseless(x)
-    return 10 * sin(4 * π * x);
-end
+    function objective_noiseless(x)
+        return 10 * sin(4.0 * π * x);
+    end
 
-samples = 10
-x = rand(Float64, samples)
-y = objective.(x)
-var_history = Array{Float64, 1}();
-mean_history = Array{Float64, 1}();
-merit_history = Array{Float64, 1}();
+    samples = 10
+    x = rand(Float64, samples)
+    y = objective.(x)
+    var_history = Array{Float64, 1}();
+    mean_history = Array{Float64, 1}();
+    merit_history = Array{Float64, 1}();
 
-plt.plot(0.0:0.001:1.0, objective_noiseless(0.0:0.001:1.0))
-plt.scatter(x, y)
-plt.show()
-
-#ccall((:build, "./libLPBO.so"), Void, (Ptr{Float64}, Ptr{Float64}, Int32), x, y, size(x)[1])
-
-resolution = 100
-x_tilda = Array{Float64}(0.0:(1/resolution):1.0)
-y_tilda = Array{Float64}(resolution + 1)
-
-model = ccall((:test_init, "./libLPBO.so"), Ptr{Void}, 
-              (Ptr{Float64}, Ptr{Float64}, Int64),
-              x, y, samples)
-
-x_selected = 0
-y_selected = 0
-
-for i = 1:30
-    mean  = Ref{Float64}(0.0);
-    merit = Ref{Float64}(0.0);
-    var   = Ref{Float64}(0.0);
-
-    x_selected = ccall((:test_next_point, "./libLPBO.so"), Float64,
-                       (Ptr{Void}, Float64, Float64, Int64, Int64,
-                        Ref{Float64}, Ref{Float64}, Ref{Float64}),
-                       model, x_selected, y_selected, i, 200, merit, mean, var)
-    y_selected = objective(x_selected)
-
-    ccall((:test_render_acquisition, "./libLPBO.so"), Float64,
-          (Ptr{Void}, Ptr{Float64}, Ptr{Float64},
-           Int64, Int64, Ptr{Float64}, Ptr{Float64}, Int64),
-          model, x, y, samples, i, x_tilda, y_tilda, resolution)
-
-    plt.plot(x_tilda, y_tilda)
-    plt.plot(0.0:0.001:1.0, objective_noiseless(0.0:0.001:1.0))
-    plt.scatter(x, y, color="blue")
-    plt.scatter(x_selected, y_selected, color="red")
+    plt.plot(0.0:0.001:1.0, objective_noiseless.(0.0:0.001:1.0))
+    plt.scatter(x, y)
     plt.show()
 
-    push!(x, x_selected)
-    push!(y, y_selected)
-    push!(mean_history, mean[])
-    push!(merit_history, merit[])
-    push!(var_history, var[])
-    samples += 1
+    #ccall((:build, "./libLPBO.so"), Void, (Ptr{Float64}, Ptr{Float64}, Int32), x, y, size(x)[1])
 
-    println("iteration: ", i, " mean: ", mean[], " var: ", var[],
-            " x: ", x_selected, " y: ", y_selected)
-end
+    resolution = 100
+    x_tilda = Array{Float64}(0.0:(1/resolution):1.0)
+    y_tilda = Array{Float64}(undef, resolution + 1)
 
-str = Vector{UInt8}(1024 * 1024);
-size = Ref{Int64}(0);
-println("serializing...")
-ccall((:test_serialize, "./libLPBO.so"), Void, (Ptr{Void}, Ref{UInt8}, Ref{Int64}), model, str, size)
-println("deserializing...")
-ccall((:test_deserialize, "./libLPBO.so"), Void, (Ptr{Void}, Ptr{UInt8}, Int64), model, str, size[])
-println("...done!")
+    model = ccall((:test_init, "./libLPBO.so"), Ptr{Cvoid}, 
+                  (Ptr{Float64}, Ptr{Float64}, Int64),
+                  x, y, samples)
 
-for i = 31:100
-    mean  = Ref{Float64}(0.0);
-    merit = Ref{Float64}(0.0);
-    var   = Ref{Float64}(0.0);
+    x_selected = 0.0
+    y_selected = 0.0
 
-    x_selected = ccall((:test_next_point, "./libLPBO.so"), Float64,
-                       (Ptr{Void}, Float64, Float64, Int64, Int64,
-                        Ref{Float64}, Ref{Float64}, Ref{Float64}),
-                       model, x_selected, y_selected, i, 200, merit, mean, var)
-    y_selected = objective(x_selected)
+    for i = 1:30
+        mean  = Ref{Float64}(0.0);
+        merit = Ref{Float64}(0.0);
+        var   = Ref{Float64}(0.0);
 
-    ccall((:test_render_acquisition, "./libLPBO.so"), Float64,
-          (Ptr{Void}, Ptr{Float64}, Ptr{Float64},
-           Int64, Int64, Ptr{Float64}, Ptr{Float64}, Int64),
-          model, x, y, samples, i, x_tilda, y_tilda, resolution)
+        x_selected = ccall((:test_next_point, "./libLPBO.so"), Float64,
+                           (Ptr{Cvoid}, Float64, Float64, Int64, Int64,
+                            Ref{Float64}, Ref{Float64}, Ref{Float64}),
+                           model, x_selected, y_selected, i, 200, merit, mean, var)
+        y_selected = objective(x_selected)
 
-    plt.plot(x_tilda, y_tilda)
-    plt.plot(0.0:0.001:1.0, objective_noiseless(0.0:0.001:1.0))
-    plt.scatter(x, y, color="blue")
-    plt.scatter(x_selected, y_selected, color="red")
+        ccall((:test_render_acquisition, "./libLPBO.so"), Float64,
+              (Ptr{Cvoid}, Ptr{Float64}, Ptr{Float64},
+               Int64, Int64, Ptr{Float64}, Ptr{Float64}, Int64),
+              model, x, y, samples, i, x_tilda, y_tilda, resolution)
+
+        # plt.plot(x_tilda, y_tilda)
+        # plt.plot(0.0:0.001:1.0, objective_noiseless.(0.0:0.001:1.0))
+        # plt.scatter(x, y, color="blue")
+        # plt.scatter(x_selected, y_selected, color="red")
+        # plt.show()
+
+        push!(x, x_selected)
+        push!(y, y_selected)
+        push!(mean_history, mean[])
+        push!(merit_history, merit[])
+        push!(var_history, var[])
+        samples += 1
+
+        println("iteration: ", i, " mean: ", mean[], " var: ", var[],
+                " x: ", x_selected, " y: ", y_selected)
+    end
+
+    str = Vector{UInt8}(undef, 1024 * 1024);
+    size = Ref{Int64}(0);
+    println("serializing...")
+    ccall((:test_serialize, "./libLPBO.so"), Cvoid, (Ptr{Cvoid}, Ref{UInt8}, Ref{Int64}), model, str, size)
+    println("deserializing...")
+    ccall((:test_deserialize, "./libLPBO.so"), Cvoid, (Ptr{Cvoid}, Ptr{UInt8}, Int64), model, str, size[])
+    println("...done!")
+
+    for i = 31:100
+        mean  = Ref{Float64}(0.0);
+        merit = Ref{Float64}(0.0);
+        var   = Ref{Float64}(0.0);
+
+        x_selected = ccall((:test_next_point, "./libLPBO.so"), Float64,
+                           (Ptr{Cvoid}, Float64, Float64, Int64, Int64,
+                            Ref{Float64}, Ref{Float64}, Ref{Float64}),
+                           model, x_selected, y_selected, i, 200, merit, mean, var)
+        y_selected = objective(x_selected)
+
+        ccall((:test_render_acquisition, "./libLPBO.so"), Float64,
+              (Ptr{Cvoid}, Ptr{Float64}, Ptr{Float64},
+               Int64, Int64, Ptr{Float64}, Ptr{Float64}, Int64),
+              model, x, y, samples, i, x_tilda, y_tilda, resolution)
+
+        plt.plot(x_tilda, y_tilda)
+        plt.plot(0.0:0.001:1.0, objective_noiseless.(0.0:0.001:1.0))
+        plt.scatter(x, y, color="blue")
+        plt.scatter(x_selected, y_selected, color="red")
+        plt.show()
+
+        push!(x, x_selected)
+        push!(y, y_selected)
+        push!(mean_history, mean[])
+        push!(merit_history, merit[])
+        push!(var_history, var[])
+        samples += 1
+        println("iteration: ", i, " mean: ", mean[], " var: ", var[],
+                " x: ", x_selected, " y: ", y_selected)
+    end
+
+    plt.plot(1:30, merit_history)
+    #plt.plot(1:30, var_history)
     plt.show()
+end 
 
-    push!(x, x_selected)
-    push!(y, y_selected)
-    push!(mean_history, mean[])
-    push!(merit_history, merit[])
-    push!(var_history, var[])
-    samples += 1
-    println("iteration: ", i, " mean: ", mean[], " var: ", var[],
-            " x: ", x_selected, " y: ", y_selected)
-end
-
-plt.plot(1:30, merit_history)
-#plt.plot(1:30, var_history)
-plt.show()
+main()
