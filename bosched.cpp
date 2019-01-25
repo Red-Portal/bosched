594968708d4cae686dad2f99b96395194aa969cd
Ray Kim <msca8h@naver.com>
2019-01-25 13:26:55 +0900

    fixing

--- bosched.cpp
+++ bosched.cpp
@@ -28,7 +28,6 @@ bool _is_bo_schedule = false;
 bool _is_new_file    = false;
 std::mt19937 _rng __attribute__((init_priority(101)));
 std::string _progname __attribute__((init_priority(101)));
-std::uniform_real_distribution<double> dist(_epsilon, 1.0);
 std::unordered_map<size_t, bosched::loop_state_t> _loop_states __attribute__((init_priority(101)));
 long _procs;
 nlohmann::json _stats;
@@ -38,6 +37,7 @@ namespace bosched
     inline double
     warmup_next_param()
     {
+        static std::uniform_real_distribution<double> dist(_epsilon, 1.0);
         double next = dist(_rng);
         return next;
     }
