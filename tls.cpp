f82a67ecd853a75bb209cab085a5f35d2016855e
Ray Kim <msca8h@naver.com>
2019-01-27 19:28:22 +0900

    the best solution: mutex

--- tls.cpp
+++ tls.cpp
@@ -7,11 +7,14 @@
 #include <boost/thread/tss.hpp>
 #include <cmath>
 
-
 #include <optional>
+#include <unordered_map>
+#include <thread>
+#include <mutex>
 
 std::atomic<size_t> _total_runtime;
-std::optional<boost::thread_specific_ptr<bosched::clock::time_point>> _start;
+std::mutex _mtx;
+std::unordered_map<std::thread::id, bosched::clock::time_point> _map;
 
 namespace bosched
 {
@@ -22,32 +25,26 @@ namespace bosched
 
     void iteration_start_record()
     {
-        if(!_start)
-        {
-            _start.emplace();
-        }
-
-        if(!_start->get())
-        {
-            _start->reset(new bosched::clock::time_point(bosched::clock::now()));
-        }
-        else
-        {
-            **_start = bosched::clock::now();
-        }
+        _mtx.lock();
+        _map[std::this_thread::get_id()] = bosched::clock::now();
+        _mtx.unlock();
     }
 
     void iteration_stop_record()
     {
-        auto duration = bosched::clock::now() - **_start;
+        _mtx.lock();
+        auto stop_point = _map[std::this_thread::get_id()];
+        _mtx.unlock();
+        
+        auto duration = bosched::clock::now() - stop_point;
         auto discrete = std::chrono::duration_cast<
             std::chrono::nanoseconds>(duration);
         _total_runtime += discrete.count();
     }
 
-    bosched::millisecond
-    fetch_total_runtime()
-    {
-        return std::chrono::nanoseconds(_total_runtime.load());
+        bosched::millisecond
+        fetch_total_runtime()
+        {
+            return std::chrono::nanoseconds(_total_runtime.load());
+        }
     }
-}
