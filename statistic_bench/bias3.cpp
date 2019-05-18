e07022ac0bd2150359202198de4d678a43d61828
Ray Kim <msca8h@naver.com>
2019-05-18 16:16:11 +0900

    uniform first

---
+++ statistic_bench/bias3.cpp
@@ -0,0 +1,69 @@
+
+#include "util.hpp"
+
+int main(int argc, char** argv)
+{
+    auto seed = std::random_device();
+    auto rng  = std::mt19937_64(seed());
+    auto str  = std::string(argv[1]);
+
+    namespace po = boost::program_options; 
+    using namespace std::literals::string_literals;
+
+    po::options_description desc("Options"); 
+    desc.add_options() 
+        ("N", po::value<int>(), "iterations") 
+        ("sigma", po::value<double>()->default_value(1.0), "variance");
+
+    po::variables_map vm;
+    try 
+        { 
+            po::store(po::command_line_parser(argc, argv)
+                      .options(desc).run(), vm);
+            po::notify(vm); 
+        } 
+    catch(po::error& e) 
+        { 
+            std::cerr << "ERROR: " << e.what() << std::endl << std::endl; 
+            std::cerr << desc << std::endl; 
+            return 1;
+        } 
+
+    int N        = vm["N"].as<int>();
+    double sigma = vm["sigma"].as<double>();
+    std::cout << "distribution,mean,+-,dist_mean,dist_stddev"  << std::endl;
+
+    {
+        auto warmup =
+            [](std::mt19937_64& rng)
+            {
+                double value;
+                double const mu    = 1;
+                double const sigma = 1;
+                do { value = stats::rnorm(mu, sigma, rng); } while(value < 0.0);
+                return value;
+            };
+        auto gen = workload(1024, warmup, rng);
+        benchmark(gen, "warmup", 10, 1);
+    }
+
+    {
+        auto gaussian =
+            [sigma](std::mt19937_64& rng)
+            {
+                double value;
+                double const mu    = 10;
+                double const sigma = sigma;
+                do
+                    {
+                        value = stats::rnorm(mu, sigma, rng);   
+                    } while(value < 0.0);
+                return value;
+            };
+        auto gen = workload_biased(N, gaussian, rng,
+                                   [N](size_t i) {
+                                       return static_cast<double>(i) / N + 1;
+                                   });
+        benchmark(gen, "bias1", 10, sigma);
+    }
+}
---
+++ statistic_bench/util.hpp
@@ -0,0 +1,175 @@
+
+#ifndef _UTIL_HPP_
+#define _UTIL_HPP_
+
+#include <iostream>
+
+#define STATS_DONT_USE_OPENMP
+#define STATS_USE_BLAZE
+
+#include "stats/include/stats.hpp"
+
+#include <algorithm>
+#include <boost/program_options.hpp>
+#include <chrono>
+#include <cmath>
+#include <numeric>
+#include <random>
+#include <string_view>
+#include <thread>
+#include <vector>
+
+template <class Tp>
+inline void do_not_optimize(Tp const& value) {
+    asm volatile("" : : "r,m"(value) : "memory");
+}
+
+template <class Tp>
+inline void do_not_optimize(Tp& value) {
+#if defined(__clang__)
+    asm volatile("" : "+r,m"(value) : : "memory");
+#else
+    asm volatile("" : "+m,r"(value) : : "memory");
+#endif
+}
+
+using duration_t = std::chrono::duration<double, std::micro>;
+
+template<typename Dist, typename Rng, typename Fun>
+class workload_biased
+{
+    std::vector<duration_t> _runtimes;
+
+public:
+    inline
+    workload_biased(size_t num_tasks,
+                    Dist dist,
+                    Rng& rng,
+                    Fun bias_f)
+        : _runtimes(num_tasks)
+    {
+        for(size_t i = 0; i < num_tasks; ++i)
+        {
+            auto runtime = dist(rng) + bias_f(i);
+            _runtimes[i] = duration_t(runtime); 
+        }
+    }
+
+    inline duration_t
+    operator[](size_t i)
+    {
+        return _runtimes[i];
+    }
+
+    inline duration_t
+    operator[](int i)
+    {
+        return _runtimes[i];
+    }
+
+    inline size_t
+    num_tasks() const
+    {
+        return _runtimes.size();
+    }
+};
+
+template<typename Dist, typename Rng>
+class workload
+{
+    std::vector<duration_t> _runtimes;
+
+public:
+    inline
+    workload(size_t num_tasks,
+             Dist dist,
+             Rng& rng)
+        : _runtimes(num_tasks)
+    {
+        for(size_t i = 0; i < num_tasks; ++i)
+        {
+            auto runtime = dist(rng);
+            _runtimes[i] = duration_t(runtime); 
+        }
+    }
+
+    inline duration_t
+    operator[](size_t i)
+    {
+        return _runtimes[i];
+    }
+
+    inline duration_t
+    operator[](int i)
+    {
+        return _runtimes[i];
+    }
+
+    inline size_t
+    num_tasks() const
+    {
+        return _runtimes.size();
+    }
+};
+
+template<typename Type, typename It>
+Type
+mean(It begin, It end, Type init)
+{
+    Type sum = std::accumulate(begin, end, init);
+    return sum / (end - begin);
+}
+
+template<typename Type, typename It>
+Type
+stddev(It begin, It end, Type mean)
+{
+    Type sum = std::accumulate(begin, end, Type(),
+                               [mean](Type sum, Type elem){
+                                   Type temp = elem - mean;
+                                   return sum + temp * temp;
+                               });
+    return sqrt(sum / (end - begin - 1));
+}
+
+template<typename Gen>
+void benchmark(Gen&& tasks,
+               std::string_view name,
+               double dist_mean,
+               double dist_stddev)
+{
+    using clock = std::chrono::steady_clock;
+    using duration_t = std::chrono::duration<double, std::milli>;
+
+    size_t num_tasks = tasks.num_tasks();
+    size_t iteration = 256;
+
+    auto measures = std::vector<double>(iteration);
+    for(size_t it = 0; it < iteration; ++it)
+        {
+            auto loop_start = clock::now();
+#pragma omp parallel for schedule(runtime)
+            for(size_t i = 0; i < num_tasks; ++i)
+            {
+                auto start = clock::now();
+                auto runtime = tasks[i];
+                auto now = clock::now();
+                while(now - start < runtime)
+                {
+                    now = clock::now();
+                    do_not_optimize(now);
+                }
+            }
+            auto loop_end = clock::now();
+            measures[it] = std::chrono::duration_cast<duration_t>(
+                loop_end - loop_start).count();
+        }
+
+    auto mu    = mean(measures.begin(), measures.end(), 0.0);
+    auto sigma = stddev(measures.begin(), measures.end(), mu);
+    auto conf  = sigma * 1.96 / sqrt(iteration);
+    std::cout << name << ',' << mu << ',' << conf << ','
+              << dist_mean << ',' << dist_stddev << std::endl;
+}
+
+#endif
