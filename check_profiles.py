48ffce0ceb621d9568f3101e8722d6beaf6c9c0f
Ray Kim <msca8h@naver.com>
2019-11-12 17:58:17 +0900

    added profile count check

---
+++ check_profiles.py
@@ -0,0 +1,15 @@
+
+import json
+import os
+import h5py
+
+def main():
+    state   = filter(lambda name: ".workload" in name, os.listdir())
+    with h5py.File(list(state)[0], "r") as f:
+        for i in f.keys():
+            if (f[i].shape[0]) < 64:
+                exit(1)
+    exit(0)
+
+if __name__ == "__main__":
+    main()
