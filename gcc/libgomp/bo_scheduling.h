f0f32522ad262cd502542855856050999f817c40
Ray Kim <msca8h@naver.com>
2019-11-23 16:25:14 +0900

    formatted code

--- gcc/libgomp/bo_scheduling.h
+++ gcc/libgomp/bo_scheduling.h
@@ -16,13 +16,13 @@ extern void bo_record_iteration_start();
 extern void bo_record_iteration_stop();
 
 extern void bo_load_data(char const* progname,
-						 size_t sched_id);
+			 size_t sched_id);
 
 extern void bo_binlpt_load_loop(unsigned long long region_id,
-								unsigned** task_map);
+				unsigned** task_map);
 
 extern void bo_hss_load_loop(unsigned long long region_id,
-							 unsigned** task_map);
+			     unsigned** task_map);
 
 extern double bo_tss_parameter(unsigned long long region_id);
 
