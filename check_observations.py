5c8ac5f5930a627cd78f4b5f314c6bf8e0fccfa4
Ray Kim <msca8h@naver.com>
2019-12-22 15:34:39 +0900

    fixed observation counter

--- check_observations.py
+++ check_observations.py
@@ -1,17 +1,17 @@
 
 import json
 import os
+import numpy as np
 
 def main():
     state   = filter(lambda name: ".bostate" in name, os.listdir())
     with open(list(state)[0], "r") as jsonf:
         bostate = json.load(jsonf)
         for i in bostate["loops"]:
-
             if not("obs_x" in i):
                 exit(1)
             else:
-                if sum([len(i["obs_x"][i]) for i in i["obs_x"]]) < 64:
+                if np.array(i["obs_x"]).flatten().shape[0] < 64:
                     exit(1)
     exit(0)
 
