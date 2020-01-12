42db0fc5b65f990986e1a98945a184bb4395008b
Ray Kim <msca8h@naver.com>
2020-01-12 20:44:03 +0900

    bumped up

--- check_profiles.py
+++ check_profiles.py
@@ -7,7 +7,7 @@ def main():
     state   = filter(lambda name: ".workload" in name, os.listdir())
     with h5py.File(list(state)[0], "r") as f:
         for i in f.keys():
-            if (f[i].shape[0]) < 64:
+            if (f[i].shape[0]) < 256:
                 exit(1)
     exit(0)
 
