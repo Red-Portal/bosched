e02b5b9c4335a66c22a8ba127b71d14b20aa81c6
Ray Kim <msca8h@naver.com>
2020-02-11 16:56:52 +0900

    improved ending condition

--- check_profiles.py
+++ check_profiles.py
@@ -7,7 +7,9 @@ def main():
     state   = filter(lambda name: ".workload" in name, os.listdir())
     with h5py.File(list(state)[0], "r") as f:
         for i in f.keys():
-            if (f[i].shape[0]) < 256:
+            if (i == "0"):
+                continue
+            if (f[i].shape[0]) < 64:
                 exit(1)
     exit(0)
 
