054e0beefaedabd2fea9774aaff1d302c608d6fe
Ray Kim <msca8h@naver.com>
2020-03-03 22:51:19 +0900

    added AMG, hpcg, miniqmc

---
+++ miniqmc/CMake/FindMKL.cmake
@@ -0,0 +1,98 @@
+# Simple file to find MKL (if available)
+# This needs a lot of work to make it robust
+INCLUDE( CheckCXXSourceCompiles )
+
+# Extremely Basic Support of common mkl module environment variables
+# or -DMKLROOT/-DMKL_HOME instead of preferred -DMKL_ROOT
+# Some Linux distributions (Ubuntu >= 18.10) provide MKL
+# headers in /usr/include/mkl, define MKL_ROOT but not used for library searching.
+if (NOT MKL_ROOT)
+  # Finding and setting the MKL_INCLUDE_DIRECTORIES
+  find_path(MKL_INCLUDE_DIRECTORIES "mkl.h"
+    HINTS ${MKLROOT} ${MKL_HOME} $ENV{MKLROOT} $ENV{MKL_ROOT} $ENV{MKL_HOME}
+    PATH_SUFFIXES include mkl)
+  string(REPLACE "/include" "" MKL_ROOT ${MKL_INCLUDE_DIRECTORIES})
+else (NOT MKL_ROOT)
+  # Finding and setting the MKL_INCLUDE_DIRECTORIES
+  set(SUFFIXES include)
+  find_path(MKL_INCLUDE_DIRECTORIES "mkl.h" HINTS ${MKL_ROOT}
+    PATH_SUFFIXES ${SUFFIXES})
+  if (NOT MKL_INCLUDE_DIRECTORIES)
+    message(FATAL_ERROR "MKL_INCLUDE_DIRECTORIES not set. \"mkl.h\" not found in MKL_ROOT/(${SUFFIXES})")
+  endif ()
+endif (NOT MKL_ROOT)
+
+if (NOT MKL_ROOT)
+  if (CMAKE_CXX_COMPILER_ID MATCHES "Intel")
+    message(FATAL_ERROR "Intel's standard compilervar.sh sets the env variable MKLROOT.\n"
+            "If you are invoking icc without the customary environment\n"
+            "you must set the the environment variable or pass cmake MKL_ROOT.")
+  else(CMAKE_CXX_COMPILER_ID MATCHES "Intel")
+    message (FATAL_ERROR "ENABLE_MKL is TRUE and mkl directory not found. Set MKL_ROOT." )
+  endif(CMAKE_CXX_COMPILER_ID MATCHES "Intel")
+endif (NOT MKL_ROOT)
+
+message("MKL_INCLUDE_DIRECTORIES: ${MKL_INCLUDE_DIRECTORIES}")
+
+if ( NOT CMAKE_CXX_COMPILER_ID MATCHES "Intel" )
+  # Finding and setting the MKL_LINK_DIRECTORIES
+  # the directory organization varies with platform and targets
+  # these suffixes are not exhaustive
+  set(MKL_FIND_LIB "libmkl_intel_lp64${CMAKE_SHARED_LIBRARY_SUFFIX}")
+  set(SUFFIXES lib lib/intel64)
+  find_library(MKL_LINK_DIRECTORIES "${MKL_FIND_LIB}" HINTS ${MKL_ROOT}
+    PATH_SUFFIXES ${SUFFIXES})
+  if (NOT MKL_LINK_DIRECTORIES)
+    message(FATAL_ERROR "MKL_LINK_DIRECTORIES not set. ${MKL_FIND_LIB} "
+      "not found in MKL_ROOT/(${SUFFIXES})")
+  endif ()
+  string(REPLACE "/${MKL_FIND_LIB}" "" MKL_LINK_DIRECTORIES ${MKL_LINK_DIRECTORIES})
+  message("MKL_LINK_DIRECTORIES: ${MKL_LINK_DIRECTORIES}")
+  set(MKL_LINKER_FLAGS "-L${MKL_LINK_DIRECTORIES} -Wl,-rpath,${MKL_LINK_DIRECTORIES}")
+  set(MKL_LIBRARIES "-lmkl_intel_lp64 -lmkl_sequential -lmkl_core -lpthread -lm -ldl")
+else ( NOT CMAKE_CXX_COMPILER_ID MATCHES "Intel" )
+  # this takes away link control for intel but is convenient
+  # perhaps we should consider dropping it since it will more or less
+  # unify the MKL setup.
+  # Note -mkl implicitly includes that icc's mkl/include
+  set(MKL_COMPILE_DEFINITIONS "-mkl")
+endif (NOT CMAKE_CXX_COMPILER_ID MATCHES "Intel" )
+
+# Check for mkl.h
+FILE( WRITE "${CMAKE_BINARY_DIR}${CMAKE_FILES_DIRECTORY}/CMakeTmp/src_mkl.cxx"
+  "#include <iostream>\n #include <mkl.h>\n int main() { return 0; }\n" )
+try_compile(HAVE_MKL ${CMAKE_BINARY_DIR}
+  ${CMAKE_BINARY_DIR}${CMAKE_FILES_DIRECTORY}/CMakeTmp/src_mkl.cxx
+  CMAKE_FLAGS
+  "-DINCLUDE_DIRECTORIES=${MKL_INCLUDE_DIRECTORIES} "
+  "-DLINK_DIRECTORIES=${MKL_LINK_DIRECTORIES}"
+  LINK_LIBRARIES "${MKL_LIBRARIES}"
+  COMPILE_DEFINITIONS "${MKL_COMPILE_DEFINITIONS}"
+  OUTPUT_VARIABLE MKL_OUT)
+if ( NOT HAVE_MKL )
+  MESSAGE( "${MKL_OUT}" )
+endif ( NOT HAVE_MKL )
+
+# Check for mkl_vml_functions.h
+FILE( WRITE "${CMAKE_BINARY_DIR}${CMAKE_FILES_DIRECTORY}/CMakeTmp/src_mkl_vml.cxx"
+  "#include <iostream>\n #include <mkl_vml_functions.h>\n int main() { return 0; }\n" )
+try_compile(HAVE_MKL_VML ${CMAKE_BINARY_DIR}
+  ${CMAKE_BINARY_DIR}${CMAKE_FILES_DIRECTORY}/CMakeTmp/src_mkl_vml.cxx
+  CMAKE_FLAGS
+  "-DINCLUDE_DIRECTORIES=${MKL_INCLUDE_DIRECTORIES} "
+  "-DLINK_DIRECTORIES=${MKL_LINK_DIRECTORIES}"
+  COMPILE_DEFINITIONS "${MKL_COMPILE_DEFINITIONS}"
+  OUTPUT_VARIABLE MKL_OUT)
+
+IF ( HAVE_MKL )
+  SET( MKL_FOUND 1 )
+  SET( MKL_FLAGS ${MKL_COMPILE_DEFINITIONS} )
+  include_directories( ${MKL_INCLUDE_DIRECTORIES} )
+  MESSAGE(STATUS "MKL found: HAVE_MKL=${HAVE_MKL}, HAVE_MKL_VML=${HAVE_MKL_VML}")
+ELSE( HAVE_MKL )
+  SET( MKL_FOUND 0 )
+  SET( MKL_FLAGS )
+  SET( MKL_LIBRARIES )
+  SET( MKL_LINKER_FLAGS )
+  MESSAGE("MKL not found")
+ENDIF( HAVE_MKL )
---
+++ miniqmc/src/Particle/DistanceTableData.h
@@ -0,0 +1,157 @@
+////////////////////////////////////////////////////////////////////////////////
+// This file is distributed under the University of Illinois/NCSA Open Source
+// License.  See LICENSE file in top directory for details.
+//
+// Copyright (c) 2016 Jeongnim Kim and QMCPACK developers.
+//
+// File developed by:
+// Jeremy McMinnis, jmcminis@gmail.com,
+//    University of Illinois at Urbana-Champaign
+// Jeongnim Kim, jeongnim.kim@gmail.com,
+//    University of Illinois at Urbana-Champaign
+// Jaron T. Krogel, krogeljt@ornl.gov,
+//    Oak Ridge National Laboratory
+// Mark A. Berrill, berrillma@ornl.gov,
+//    Oak Ridge National Laboratory
+//
+// File created by:
+// Jeongnim Kim, jeongnim.kim@gmail.com,
+//    University of Illinois at Urbana-Champaign
+////////////////////////////////////////////////////////////////////////////////
+
+#ifndef QMCPLUSPLUS_DISTANCETABLEDATAIMPL_H
+#define QMCPLUSPLUS_DISTANCETABLEDATAIMPL_H
+
+#include "Particle/ParticleSet.h"
+#include "Utilities/PooledData.h"
+#include "Numerics/OhmmsPETE/OhmmsVector.h"
+#include "Numerics/OhmmsPETE/OhmmsMatrix.h"
+#include "Utilities/SIMD/allocator.hpp"
+#include <Numerics/Containers.h>
+#include <limits>
+#include <bitset>
+
+namespace qmcplusplus
+{
+/** enumerator for DistanceTableData::DTType
+ *
+ * - DT_AOS Use original AoS type
+ * - DT_SOA Use SoA type
+ * - DT_AOS_PREFERRED Create AoS type, if possible.
+ * - DT_SOA_PREFERRED Create SoA type, if possible.
+ * The first user of each pair will decide the type of distance table.
+ * It is the responsibility of the user class to check DTType.
+ */
+enum DistTableType
+{
+  DT_AOS = 0,
+  DT_SOA,
+  DT_AOS_PREFERRED,
+  DT_SOA_PREFERRED
+};
+
+/** @ingroup nnlist
+ * @brief Abstract class to manage pair data between two ParticleSets.
+ *
+ * Each DistanceTableData object is fined by Source and Target of ParticleSet
+ * types.
+ */
+struct DistanceTableData
+{
+  constexpr static unsigned DIM = OHMMS_DIM;
+
+  /**enum for index ordering and storage.
+   *@brief Equivalent to using three-dimensional array with (i,j,k)
+   * for i = source particle index (slowest),
+   *     j = target particle index
+   *     k = copies (walkers) index.
+   */
+  enum
+  {
+    WalkerIndex = 0,
+    SourceIndex,
+    VisitorIndex,
+    PairIndex
+  };
+
+  using IndexType       = QMCTraits::IndexType;
+  using RealType        = QMCTraits::RealType;
+  using PosType         = QMCTraits::PosType;
+  using IndexVectorType = aligned_vector<IndexType>;
+  using ripair          = std::pair<RealType, IndexType>;
+  using RowContainer    = VectorSoAContainer<RealType, DIM>;
+
+  /// type of cell
+  int CellType;
+  /// Type of DT
+  int DTType;
+  /// size of indicies
+  TinyVector<IndexType, 4> N;
+
+  /**defgroup SoA data */
+  /*@{*/
+  /** Distances[i][j] , [Nsources][Ntargets] */
+  Matrix<RealType, aligned_allocator<RealType>> Distances;
+
+  /** Displacements[Nsources]x[3][Ntargets] */
+  std::vector<RowContainer> Displacements;
+
+  /// actual memory for Displacements
+  aligned_vector<RealType> memoryPool;
+
+  /** temp_r */
+  aligned_vector<RealType> Temp_r;
+
+  /** temp_dr */
+  RowContainer Temp_dr;
+
+  /** true, if full table is needed at loadWalker */
+  bool Need_full_table_loadWalker;
+  /*@}*/
+
+  /// name of the table
+  std::string Name;
+  /// constructor using source and target ParticleSet
+  DistanceTableData(const ParticleSet& source, const ParticleSet& target)
+      : Origin(&source), N(0), Need_full_table_loadWalker(false)
+  {}
+
+  /// virutal destructor
+  virtual ~DistanceTableData() {}
+
+  /// return the name of table
+  inline std::string getName() const { return Name; }
+  /// set the name of table
+  inline void setName(const std::string& tname) { Name = tname; }
+
+  /// returns the reference the origin particleset
+  const ParticleSet& origin() const { return *Origin; }
+  inline void reset(const ParticleSet* newcenter) { Origin = newcenter; }
+
+  inline bool is_same_type(int dt_type) const { return DTType == dt_type; }
+
+  /// returns the number of centers
+  inline IndexType centers() const { return Origin->getTotalNum(); }
+
+  /// returns the number of centers
+  inline IndexType targets() const { return N[VisitorIndex]; }
+
+  /// returns the size of each dimension using enum
+  inline IndexType size(int i) const { return N[i]; }
+
+  /// evaluate the Distance Table using only with position array
+  virtual void evaluate(ParticleSet& P) = 0;
+
+  /// evaluate the Distance Table
+  virtual void evaluate(ParticleSet& P, int jat) = 0;
+
+  /// evaluate the temporary pair relations
+  virtual void move(const ParticleSet& P, const PosType& rnew) = 0;
+
+  /// update the distance table by the pair relations
+  virtual void update(IndexType jat) = 0;
+
+  const ParticleSet* Origin;
+};
+} // namespace qmcplusplus
+#endif
