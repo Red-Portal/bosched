3345457bf3f5031779cbabbbdd9002f3eedf390d
Ray Kim <msca8h@naver.com>
2020-03-27 20:31:55 +0900

    debug commit

--- utils.hpp
+++ utils.hpp
@@ -255,7 +255,7 @@ namespace bosched
 
 	    for (j = 0; j < chunksizes[sortmap[i - 1]]; j++)	
 		taskmap[chunkoff[sortmap[i - 1]] + j] = tid;	
-	    load[tid] += chunks[sortmap[i - 1]];	
+	    load[tid] += chunks[i - 1];//sortmap[i - 1]];	
 	}	
 
 	auto load2 = std::vector<size_t>(nthreads, 0.0); /* Assigned load.    */	
